03bfe6f51a7e0d86b0cff24e5bb5706d6f73b8bf
dWX989915 <dengxinli@huawei.com>
2021-04-20 16:25:36 +0800

    IE  promise 更正

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     parserOptions: {
         parser: "babel-eslint"
     },
-    globals: { d3: true },
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
